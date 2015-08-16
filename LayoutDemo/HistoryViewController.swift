6f3fa12f6ffb9b7f1597882c7f27d0cecbd7b6fc
祝文博 <wenbo.zhu@tqmall.com>
2015-08-16 23:32:27 +0800

    modify some info

--- LayoutDemo/HistoryViewController.swift
+++ LayoutDemo/HistoryViewController.swift
@@ -54,14 +54,14 @@ class HistoryViewController: UITableViewController {
         cell.timeLabel.text = "2015-07-31 02:16~2015-07-31 02:20"
         
         cell.locationImageView.image = UIImage(named: "icon_location_blue")
-        cell.locationLabel.text = "余杭区文一西路998号海创园18号楼708室"
+        cell.locationLabel.text = "some address some address some address "
         
         cell.requireImageView.image = UIImage(named: "icon_require_blue")
         cell.requireLabel.text = "雪顶咖啡不要溢出来，全家桶的可乐不加冰"
         
         cell.avatorImageView.image = UIImage(named: "icon_content_buy")
-        cell.nameLabel.text = "昂他妹"
-        cell.phoneLabel.text = "13191491234"
+        cell.nameLabel.text = "Kevin"
+        cell.phoneLabel.text = "133333333333"
         cell.publishTimeLabel.text = "发布时间 2015-08-06 01:21:30"
         
         cell.statusLabel.text = "顺利完成"
